af98289dee2374dd0d364a7104f42f0cbdb9d3bd
Stas Stepaniuk <ssttaass0415@gmail.com>
2023-12-15 16:45:29 +0200

    Added error handling

--- app.js
+++ app.js
@@ -14,4 +14,19 @@ app.use('/users', routes.user);
 app.use('/workspaces', routes.workspace);
 app.use('/workflows', routes.workflow);
 
+app.use((req, res, next) => {
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    })
+});
+
 module.exports = app;
